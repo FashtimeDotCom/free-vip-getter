1f5a62d5b90368ccba19e1d93cb40d493c6cdd1e
fourstring <odyssey471@gmail.com>
2015-08-27 22:18:10 +0800

    upload msg

---
+++ aqy.py
@@ -0,0 +1,44 @@
+﻿#coding: utf-8
+__author__ = 'AS126'
+
+from html.parser import HTMLParser
+from urllib.request import urlopen
+import re
+
+class spider(HTMLParser):
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.link=[]
+        self.data_list=[]
+    def handle_starttag(self, tag, attrs):
+        title=False
+        link_re='.*爱奇艺vip.*'
+        if tag == 'a':
+            if len(attrs) == 0:
+                pass
+            else:
+                for (name,value) in attrs:
+                    if name=='title':
+                        if re.search(link_re,value):
+                            title=True
+                for (name,value) in attrs:
+                    if name=='href' and title:
+                        self.link.append(value)
+    def handle_data(self, data):
+        data_re='账号\d*'
+        if re.match(data_re,data):
+            self.data_list.append(data)
+
+if __name__ == '__main__':
+    html=urlopen('http://www.vipfenxiang.com/aiqiyi/').read().decode('utf-8')
+    spider1=spider()
+    spider1.feed(html)
+    r1=spider1.link[0]
+    vip_page=urlopen(r1).read().decode('utf-8')
+    spider2=spider()
+    spider2.feed(vip_page)
+    for data in spider2.data_list:
+        print(data)
+    print('数据来源：VIP分享网 http://www.vipfenxiang.com/')
+    spider1.close()
+    spider2.close()
---
+++ xl.py
@@ -0,0 +1,36 @@
+﻿#coding: utf-8
+__author__ = 'AS126'
+
+from html.parser import HTMLParser
+from urllib.request import urlopen
+import re
+
+class spider(HTMLParser):
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.link=[]
+    def handle_starttag(self, tag, attrs):
+        title=False
+        link_re='.*迅雷会员.*'
+        if tag == 'a':
+            if len(attrs) == 0:
+                pass
+            else:
+                for (name,value) in attrs:
+                    if name=='title':
+                        if re.search(link_re,value):
+                            title=True
+                for (name,value) in attrs:
+                    if name=='href' and title:
+                        self.link.append(value)
+
+if __name__ == '__main__':
+    html=urlopen('http://www.vipfx8.com').read().decode('utf-8')
+    spiders=spider()
+    spiders.feed(html)
+    result=spiders.link[1].lstrip('http://www.vipfx8.com/')
+    result=result.rstrip('.html')
+    spiders.close()
+    xlvip_file=urlopen('http://www.vipfx8.com/down?down=' + result).read().decode('gbk')
+    print(xlvip_file)
+    print('数据来源：VIP分享吧 http://www.vipfx8.com')
